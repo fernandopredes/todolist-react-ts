ca90be6e86f8c40a1142cf5e51a8bea2e9682141
Fernando Predes <fernando_predes@hotmail.com>
2022-10-05 10:54:29 -0300

    css do formulário

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
-
+import styles from "./TaskForm.module.css"
 type Props = {
   btnText: string
 }
 
 const TaskForm = ({btnText}: Props) => {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.input_container}>
         <label htmlFor="title"></label>
         <input type="text" name='title' placeholder='Título da tarefa' />
       </div>
-      <div>
+      <div className={styles.input_container}>
         <label htmlFor="level"></label>
         <input type="text" name='level' placeholder='Dificuldade da tarefa' />
       </div>
