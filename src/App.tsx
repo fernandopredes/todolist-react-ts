4943d2bd7fcdd0f48bb529585cac6a1cd8c0a0a6
fernando predes <fernando_predes@hotmail.com>
2022-10-04 21:20:00 -0300

    header, footer e css deles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main className={styles.main}>
+        <h1>Conteúdo</h1>
+      </main>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import styles from "./Footer.module.css";
+
+
+const Footer = () => {
+  return (
+      <footer className={styles.footer}>
+
+          <span>todo list @2022</span>
+
+      </footer>
+  )
+}
+
+export default Footer
